26e739c16083863743450a4b8692bd19312469d5
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-03-23 18:53:12 +0900

    [Silver V] Title: 숫자 카드, Time: 732 ms, Memory: 115380 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -10,9 +10,8 @@ M=int(input())
 M_list=list(map(int,input().split()))      # 10 9 -5 2 3 4 5 -10
 
 dic={}
-for i in range(N):
-    dic[N_list[i]]=0
-    #dic={ 10:1, 9:0, -5:0, 2:1, 3:1, 4:0, 5:0, -10:0 }
+for i in N_list:
+    dic[i]=0                               #dic 배열 0으로 초기화
 
 for i in M_list:
     if i in dic:
