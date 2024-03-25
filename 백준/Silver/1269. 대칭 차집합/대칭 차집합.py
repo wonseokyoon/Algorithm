72d34262455731aa2cfddc476e25b15306120123
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-03-25 21:39:17 +0900

    [Silver IV] Title: 대칭 차집합, Time: 264 ms, Memory: 86940 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -3,25 +3,12 @@ input=sys.stdin.readline
 
 A,B=map(int,input().split())
 
-a_list=list(map(int,input().split()))
-b_list=list(map(int,input().split()))
+a_set=set(map(int,input().split()))
+b_set=set(map(int,input().split()))
 
-a_b={}  #a-b
-b_a={}  #b-a
-for i in a_list:
-    a_b[i]=0
-for i in b_list:
-    b_a[i]=0
 
-a_cnt=0
-b_cnt=0
-for i in b_list:
-    if i in a_b:
-        a_cnt+=1
+diff_ab=a_set - b_set
+diff_ba=b_set - a_set
 
-for i in a_list:
-    if i in b_a:
-        b_cnt+=1
-
-print(A+B-a_cnt-b_cnt)
+print(len(diff_ab)+len(diff_ba))
 
