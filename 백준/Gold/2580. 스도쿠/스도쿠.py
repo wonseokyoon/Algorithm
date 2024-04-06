350790d5d02c035f216b6738f32770dc092e0388
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-04-06 15:40:02 +0900

    [Gold IV] Title: 스도쿠, Time: 3640 ms, Memory: 140264 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -3,35 +3,36 @@ input=sys.stdin.readline
 #import queue
 from collections import deque
 
+
 def is_valid(board,row,col,num):
-    # 행에 중복된 숫자가 있는지 확인
+    #행일치
     for i in range(9):
         if board[row][i]==num:
             return False
-    # 열에 중복된 숫자가 있는지 확인
-    for i in range(9):
-        if board[i][col]==num:
+    #열일치
+    for j in range(9):
+        if board[j][col]==num:
             return False
-    # 작은 사각형에 중복된 숫자가 있는지 확인
-    small_start_row,small_start_col=3*(row//3), 3*(col//3)
-    #작은 사각형 내부의 시작좌표(0,0)를 재 설정
+    #작은 사각형 일치
+    start_small_row,start_small_col=3*(row//3),3*(col//3)
     for row in range(3):
         for col in range(3):
-            if board[small_start_row+row][small_start_col+col]==num:    
+            if board[start_small_row+row][start_small_col+col]==num:
                 return False
     return True
 
-def sudoku(board):
-    if not is_empty(board):     #보드를 가득 채운 상태
-        return True
-    row,col=is_empty(board)     # 빈칸의 행과 열 저장
 
+def sudoku(board):
+    if not is_empty(board):
+        return True
+    row,col=is_empty(board)
+    
     for num in range(1,10):
-        if is_valid(board,row,col,num):     # 해당 숫자가 빈칸에 들어갈 수 있으면
-            board[row][col]=num             # 해당 숫자 입력
-            if sudoku(board):               # 슈도쿠가 완성이 됐다면(동시에 재귀호출)
+        if is_valid(board,row,col,num):
+            board[row][col]=num
+            if sudoku(board):
                 return True
-            board[row][col]=0               # 빈칸에 숫자가 못들어 갈 경우(잘못된 case)에 백트래킹
+            board[row][col]=0
     return False
 
 def is_empty(board):
@@ -39,16 +40,16 @@ def is_empty(board):
         for j in range(9):
             if board[i][j]==0:
                 return i,j
-    
 
 
 
 
 board=[]
-for _ in range(9):
+for i in range(9):
     row=list(map(int,input().split()))
     board.append(row)
 
 sudoku(board)
+
 for row in board:
     print(' '.join(map(str,row)))
