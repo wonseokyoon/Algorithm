be24e513df1261ce25f06040ebd8ab20faaae9a5
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-21 16:50:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 14.10 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
 class Solution {
     public String solution(String[] seoul) {
-        for(int i=0;i<seoul.length;i++){
-            if(seoul[i].equals("Kim")) {
-                return "김서방은 " + i + "에 있다";
-            }
-        }
-        return "";
+        int x=Arrays.asList(seoul).indexOf("Kim");  //Kim의 인덱스 탐색
+        return "김서방은 " + x + "에 있다";
     }
 }
