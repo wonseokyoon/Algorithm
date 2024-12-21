a40a5e3edabb15c6ea9b3f553f6e4e8f8aa77e7e
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-21 14:40:12 +0900

    [level 1] Title: 하샤드 수, Time: 0.18 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,24 +1,14 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public boolean solution(int x) {
         boolean answer = false;
-        String strx=Integer.toString(x);    // x의 문자열 변환
-        int cnt=strx.length();      // 몇자리 숫자인지 확인
-        List<Integer> digits=new ArrayList<>();
+        // x를 배열에 저장
+        String [] digits=String.valueOf(x).split("");
         int num=x;
-        for(int i=0;i<cnt;i++){
-            digits.add(num%10);   //4,3,2,1
-            num/=10;
-        }
         int sum=0;
-        for(int digit:digits){  // 각 자리의 합계
-            sum+=digit;
+        for(String digit:digits){
+            sum+=Integer.parseInt(digit);   // 배열 순회하면서 합
         }
-        if(x%sum==0){   //하샤드(나누어지는지) 확인
-            answer=true;
-        }
+        if(x%sum==0) answer=true;
         return answer;
     }
 }
