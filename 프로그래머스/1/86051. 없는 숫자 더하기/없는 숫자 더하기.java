0685c4e58e4692c8014b15045d798c0bf2eac33f
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-23 00:05:49 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.83 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,9 +1,6 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] numbers) {
-        int sum=0;
-        for(int num:numbers){
-            sum+=num;
-        }
-        return 45-sum;
+        return 45-Arrays.stream(numbers).sum();
     }
 }
