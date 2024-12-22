ca7c318779ac1e75cb3dbb26fcc4e00c84947dd1
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-22 17:37:33 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.05 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,19 +1,12 @@
 class Solution {
     public int solution(int num) {
-        long numl=num;
-        int cnt=0;
-        if(numl==1) return 0;
-        while (numl > 1) {
-            if (numl % 2 == 0) {
-                numl /= 2;
-            } else {
-                numl = numl * 3 + 1;
-            }
-            cnt++;
-
-            if (cnt > 500) return -1; // 500번 이상 반복 시 종료
+        long n=num;
+        if(n==1) return 0;
+        for(int i=0;i<500;i++){
+            n=(n%2==0)?n/2:n*3+1;
+            if(n==1) return i+1;
         }
-        return cnt;
+        return -1;
 
     }
 }
