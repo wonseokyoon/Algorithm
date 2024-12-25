8beaf562a26b2ede8dfcce8dea112c60194de3ee
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-25 23:58:14 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 2.03 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,24 +1,16 @@
 import java.util.ArrayList;
 class Solution {
     public int solution(int left, int right) {
-        ArrayList<Integer> sum=new ArrayList<>();
-        for(int i=left;i<=right;i++){
-            int cnt=0;
-            for(int j=1;j<=Math.sqrt(i);j++){
-                if(i%j==0){ //나눠지는 경우
-                    cnt+=2;
-                }
-                if(j==Math.sqrt(i)) cnt--;
+         ArrayList<Integer> sum=new ArrayList<>();
+        for(int i=left;i<=right;i++) {
+            if(i%Math.sqrt(i)==0){  //제곱수: 약수 개수 홀수개
+                sum.add(-i);
             }
-            System.out.println(cnt);
-            if(cnt%2==0){ //약수 개수 짝수
+            else{
                 sum.add(i);
             }
-            else{   //약수 개수 홀수
-                sum.add(-1*i);
-            }
         }
-
         return sum.stream().mapToInt(Integer::intValue).sum();
+    
     }
 }
