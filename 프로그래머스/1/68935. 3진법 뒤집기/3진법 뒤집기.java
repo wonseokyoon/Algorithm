9311cba4c7634b07b6e126d20fb8b29fbc6f8fac
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-28 17:15:23 +0900

    [level 1] Title: 3진법 뒤집기, Time: 13.72 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,25 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 class Solution {
     public int solution(int n) {
-        int div=1; //몫
-        int mod=1; // 나머지
-        ArrayList<Integer> num3=new ArrayList<>();
-        while(div>0){
-            div=n/3;
-            mod=n%3;
+        String str="";
+        while(n>0){
+            str=(n%3)+str; //1 1 2 0
             n/=3;
-            num3.add(mod);
         }
-        int ans=0;
-        int cnt=0;
-        Collections.reverse(num3);
-        //System.out.println(Arrays.toString(num3.stream().mapToInt(Integer::intValue).toArray()));
-        for(int num:num3){
-            ans+=num*Math.pow(3,cnt);
-            cnt++;
-        }
-        return ans;
-
+        List<String> answer=new ArrayList<>(Arrays.asList(str.split("")));
+        Collections.reverse(answer);
+        return Integer.parseInt(String.join("",answer),3);
     }
 }
