b71233bae134d9ec2fdd5efda327b82bb7503551
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-28 16:00:41 +0900

    [level 1] Title: 예산, Time: 1.19 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,18 +1,14 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] d, long budget) {
-        Arrays.sort(d); //정렬하여 최대한 많은 금액 지원
+        Arrays.sort(d);
         long remain = budget;  //남은 돈
         int cnt=0;
         for (int i = 0; i < d.length; i++) {
-            int amount = d[i];
-            remain-=amount;
-            if(remain==0){
-                return i+1;
-            } else if (remain<0) {
-                return i;
-            }
+            remain-=d[i];
+            if(remain<0) return cnt;
+            cnt++;
         }
-        return d.length;
+        return cnt;
     }
 }
