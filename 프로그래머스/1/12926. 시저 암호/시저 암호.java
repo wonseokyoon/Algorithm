348ff33c1f233d06467c0d440a672160e66f5ce7
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-29 14:49:22 +0900

    [level 1] Title: 시저 암호, Time: 1.74 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,30 +1,12 @@
 class Solution {
-    //소문자: 97~122
-    //대문자: 65~90
     public String solution(String s, int n) {
-        int ascii;
         StringBuilder sb=new StringBuilder();
         for(char c:s.toCharArray()){    //ABZ
-            ascii=(int)c;   //아스키코드
             if(Character.isUpperCase(c)){   //대문자
-                if(ascii+n>90){ // 범위를 벗어나는 경우
-                    ascii=ascii+n-26;
-                    c=(char)ascii;   // 문자로 변환
-                }
-                else{
-                    ascii+=n;
-                    c=(char)ascii;
-                }
+               c=(char)((c-'A'+n)%26+'A');
             }
             else if(Character.isLowerCase(c)){   //소문자
-                if(ascii+n>122){
-                    ascii=ascii+n-26;
-                    c=(char)ascii;
-                }
-                else{
-                    ascii+=n;
-                    c=(char)ascii;
-                }
+                c=(char)((c-'a'+n)%26+'a');
             }
             sb.append(c);
         }
