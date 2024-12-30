73bdad68900d88296354e5f9eb0e062153c86976
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2024-12-30 17:01:42 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.59 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,20 +1,11 @@
-import java.util.*;
 class Solution {
-    public int solution(String s) {
-        HashMap<String,Integer> map=new HashMap<>();
+        public int solution(String s) {
         String[] str={"zero","one","two","three","four","five"
         ,"six","seven","eight","nine"};
-        for (int i=0;i<10;i++){
-            map.put(str[i],i);  // zero:0, one:1, two:2, ...
+        String[] digit={"0","1","2","3","4","5","6","7","8","9"};
+        for(int i=0;i<str.length;i++){
+            s=s.replaceAll(str[i],digit[i]);
         }
-        StringBuilder sb=new StringBuilder(s);
-        for(String key:map.keySet()){
-            while(sb.indexOf(key) != -1){
-                int idx=sb.indexOf(key);    //대체 문자열의 시작 인덱스
-                int value=map.get(key); // value
-                sb.replace(idx,idx+key.length(),String.valueOf(value));
-            }
-        }
-        return Integer.parseInt(sb.toString());
+        return Integer.parseInt(s);
     }
 }
