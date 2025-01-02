e4f64b0d94505508c806ec66cda04b519c19c4b3
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-02 21:43:20 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        ArrayList<Integer> klist=new ArrayList<>();
+        int[] answer=new int[commands.length];
 
         for(int i=0;i< commands.length;i++){
             int[] temp=Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
             Arrays.sort(temp);  // 2 3 5 6
-            klist.add(temp[commands[i][2]-1]);
+            answer[i]=temp[commands[i][2]-1];
         }
-        return klist.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
     }
 }
