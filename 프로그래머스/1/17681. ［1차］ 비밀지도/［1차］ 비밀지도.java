21b8c85195fb9bf884a344ec988d5c6d72db238e
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-04 15:27:36 +0900

    Update ［1차］ 비밀지도.java

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,36 +1,17 @@
-import java.util.*;
 class Solution {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
-        String s=Integer.toBinaryString(30);
+ public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] result = new String[n];
 
         for(int i=0;i<n;i++){
-            StringBuilder temp=new StringBuilder();
-            String sb1=Integer.toBinaryString(arr1[i]);
-            String sb2=Integer.toBinaryString(arr2[i]);
+            result[i]=Integer.toBinaryString(arr1[i]|arr2[i]);
 
             // 두 이진 문자열 길이 맞추기 위해 패딩
-            while(sb1.length()<n){
-                sb1="0"+sb1;
-            }
-            while(sb2.length()<n){
-                sb2="0"+sb2;
-            }
-
-            for(int j=0;j< sb1.length();j++){
-                if(sb1.charAt(j)=='1' || sb2.charAt(j)=='1'){
-                    temp.append('#');
-                }
-                else{
-                    temp.append(' ');
-                }
-            }
-
-
-            answer[i]=temp.toString();
+            result[i]=String.format("%"+n+"s",result[i]);
+            result[i]=result[i].replaceAll("1","#");
+            result[i]=result[i].replaceAll("0"," ");
         }
 
 
-        return answer;
+        return result;
     }
 }
