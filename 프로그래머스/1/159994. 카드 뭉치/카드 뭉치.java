9604973279e4b37e76d34bfea199a1429b5ef8b3
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-05 11:54:24 +0900

    [level 1] Title: 카드 뭉치, Time: 0.14 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,21 +1,28 @@
 import java.util.*;
 class Solution {
 public String solution(String[] cards1, String[] cards2, String[] goal) {
-        ArrayList<String> card1=new ArrayList<>(Arrays.asList(cards1));
-        ArrayList<String> card2=new ArrayList<>(Arrays.asList(cards2));
-
+        Queue<String> card1=new LinkedList<>();
+        Queue<String> card2=new LinkedList<>();
+        Queue<String> goals=new LinkedList<>();
+        for(String str:cards1){
+            card1.offer(str);
+        }for(String str:cards2){
+            card2.offer(str);
+        }for(String str:goal){
+            goals.offer(str);
+        }
         boolean result=true;
-        for(int i=0;i< goal.length;i++){
-            if(!card1.isEmpty()&&goal[i].equals(card1.get(0))){
-                card1.remove(0);
-            } else if (!card2.isEmpty()&&goal[i].equals(card2.get(0))) {
-                card2.remove(0);
+        while(!goals.isEmpty()){
+            if(goals.peek().equals(card1.peek())&& !card1.isEmpty()){
+                card1.poll();
+                goals.poll();
+            } else if (goals.peek().equals(card2.peek()) && !card2.isEmpty()) {
+                card2.poll();
+                goals.poll();
             }else{
-                result=false;
-                break;
+                return "No";
             }
         }
-
-        return result?"Yes":"No";
+        return "Yes";
     }
 }
