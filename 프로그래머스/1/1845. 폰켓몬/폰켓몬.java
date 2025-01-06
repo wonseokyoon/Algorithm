4d8ffb561ea33732d9679feb16625f0ef784ab7f
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-06 17:22:23 +0900

    [level 1] Title: 폰켓몬, Time: 6.65 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,14 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] nums) {
+public int solution(int[] nums) {
         int n= nums.length/2;   //뽑아야하는 포켓몬 수
-
-        int[] unique=Arrays.stream(nums).distinct().toArray();
-
-        if(unique.length<n){
-            return unique.length;
+        
+        HashSet<Integer> unique=new HashSet<>();
+        for(int num:nums){
+            unique.add(num);
+        }
+        if(unique.size()<n){
+            return unique.size();
         }
         else{
             return n;
