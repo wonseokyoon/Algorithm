e94d9fde4dea3249bfb5ef71416b867eacf41820
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-06 18:56:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 10.96 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,21 +1,22 @@
 import java.util.*;
 class Solution {
-public int solution(int number, int limit, int power) {
-        ArrayList<Integer> answer=new ArrayList<>();
+ public int solution(int number, int limit, int power) {
+        int[] count=new int[number+1];
         for(int i=1;i<=number;i++){
-            int cnt=0;
-            int num= (int) Math.sqrt(i);    //2
-            while(num>=1){
-                if(i%num==0){   // 나머지 0
-                    cnt++;
-                    if(i/num !=num){    //제곱근이면 카운트-1
-                        cnt++;
-                    }
-                }
-                num--;
+            for(int j=1;j<=number/i;j++){
+                count[i*j]++;
             }
-            answer.add((cnt>limit)?power:cnt);
         }
-        return answer.stream().mapToInt(Integer::intValue).sum();
+        int sum=0;
+        for(int n:count){
+            if(n>limit){
+                sum+=power;
+            }
+            else{
+                sum+=n;
+            }
+        }
+        return sum;
+
     }
 }
