ce92bbf992bd812b2b7d972e3de61e71b3c854c5
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-10 15:46:18 +0900

    [level 1] Title: 옹알이 (2), Time: 1.25 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,41 +1,22 @@
 class Solution {
-    public int solution(String[] babbling) {
-        String[] origin = {"aya", "ye", "woo", "ma"};
-        int cnt = 0;
-
-        for (String str : babbling) {
-            String prev = ""; // 이전 발음
-            boolean isValid = true;
-
-            // 문자열이 공백이 아닐때까지 수행
-            while (!str.isEmpty()) {
-                boolean found = false;
-
-                for (String word : origin) {
-                    if (str.startsWith(word)) { // 단어가 문자열의 시작과 일치하는 경우
-                        if (prev.equals(word)) { // 이전 발음과 동일하면 무효
-                            isValid = false;
-                        } else {
-                            prev = word; // 이전 발음 갱신
-                        }
-                        str = str.replaceFirst(word, ""); // 발음 제거
-                        found = true; // 발음이 발견됨
-                        break; // 일치하는 첫 번째 발음만 처리한 후 종료
-                    }
-                }
-
-                if (!found) { // 어떤 발음도 발견되지 않았다면 종료
-                    isValid = false;
-                    break;
-                }
+     public int solution(String[] babblings) {
+        int answer=0;
+        for(int i=0;i<babblings.length;i++){
+            if(babblings[i].contains("ayaaya")
+                    || babblings[i].contains("yeye")
+                    || babblings[i].contains("woowoo")
+                    || babblings[i].contains("mama")){
+                continue;
             }
+            babblings[i] = babblings[i].replace("aya", " ");
+            babblings[i] = babblings[i].replace("ye", " ");
+            babblings[i] = babblings[i].replace("woo", " ");
+            babblings[i] = babblings[i].replace("ma", " ");
+            babblings[i] = babblings[i].replace(" ", "");
 
-            // 남은 문자열이 비어 있어야 유효한 발음으로 판단
-            if (isValid && str.isEmpty()) {
-                cnt++;
-            }
+            if(babblings[i].length()  == 0) answer++;
         }
+        return answer;
 
-        return cnt;
     }
 }
