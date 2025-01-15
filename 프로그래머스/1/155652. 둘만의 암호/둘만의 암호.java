c1548641fd65cbb1c63f88e3890e3a4179405774
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-15 12:16:26 +0900

    [level 1] Title: 둘만의 암호, Time: 1.13 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -4,24 +4,18 @@ import java.util.List;
 
 class Solution {
      public String solution(String s, String skip, int index) {
-        String alpabet="abcdefghijklmnopqrstuvwxyz";
-        List<String> list= new ArrayList<>(Arrays.asList(alpabet.split("")));
-        System.out.println(list);
-        String[] skiplist=skip.split("");
-        for(String c:skiplist){ // skip 제거
-            if(list.contains(c)){
-                list.remove(c);
+        StringBuilder answer = new StringBuilder();
+
+        for (char letter : s.toCharArray()) {
+            int cnt = 0;
+            while (cnt < index) {   // index번 반복
+                letter = (letter == 'z') ? 'a' : (char) (letter + 1);
+                if (!skip.contains(String.valueOf(letter))) {
+                    cnt++;
+                }
             }
+            answer.append(letter);
         }
-        int size= list.size();
-        StringBuilder sb=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            int afterindex=list.indexOf(String.valueOf(s.charAt(i)));
-            afterindex=(afterindex+index)%size;
-            sb.append(list.get(afterindex));
-        }
-
-        return sb.toString();
-
+        return answer.toString();
     }
 }
