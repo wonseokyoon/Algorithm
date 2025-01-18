56f404c26d419cbc2fb6c8bfb1fdd84dd34f237b
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-18 18:07:32 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,11 +1,9 @@
 class Solution {
-    public long solution(long n) {
-        double sq=Math.sqrt(n);
-        if(sq%1==0){   //제곱근이 있는 경우
-            return (long) Math.pow(sq+1,2);
+  public long solution(long n) {
+      if (Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
         }
-        else{
-            return -1;
-        }
-    }
+
+        return -1;
+  }
 }
