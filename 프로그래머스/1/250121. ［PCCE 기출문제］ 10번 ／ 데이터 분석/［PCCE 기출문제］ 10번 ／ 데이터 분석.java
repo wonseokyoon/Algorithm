a3c0350fe8e1a4fc7d389ff99846789561ab101d
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-27 11:19:04 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 8.71 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,27 +1,15 @@
 import java.util.*;
 class Solution {
-    public List<int[]> solution(int[][] data, String ext, int val_ext, String sort_by) {
-        List<int[]> list=new ArrayList<>();
-        HashMap<String,Integer> map=new HashMap<>();
-        map.put("code",0);
-        map.put("date",1);
-        map.put("maximum",2);
-        map.put("remain",3);
-        int sidx=-1;
-        int idx=-1;
-        for(String str: map.keySet()){
-            if(ext.equals(str)){
-                idx=map.get(ext);
-            }
-            if(sort_by.equals(str)){
-                sidx=map.get(str);
-            }
-        }
-        final int sort_idx=sidx;
-        for(int i=0;i< data.length;i++){
-            if(data[i][idx]<val_ext) list.add(data[i]);
-        }
-        list.sort((o1,o2)->Integer.compare(o1[sort_idx],o2[sort_idx]));
-        return list;
+   public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+
+        String[] command={"code","date","maximum","remain"};
+        List<String> list=Arrays.asList(command);
+
+        int val1=list.indexOf(ext);
+        int val2=list.indexOf(sort_by);
+
+        int[][] answer= Arrays.stream(data).filter(o1 -> o1[val1]<val_ext)
+                .sorted((o1, o2) -> o1[val2]-o2[val2]).toArray(int[][]::new);
+        return answer;
     }
 }
