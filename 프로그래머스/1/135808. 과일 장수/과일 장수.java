637546bb540b88c4a2a7d99163de7eb811e63281
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-01-30 20:25:52 +0900

    [level 1] Title: 과일 장수, Time: 64.76 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        Arrays.sort(score); //오름차순 정렬
-        // 1 1 2 2 2 2 4 4 4 4 4 4
-        int answer=0;
-        for(int i= score.length;i>=m;i-=m){
-            answer+=score[i-m]*m;
+        int answer = 0;
+
+        Arrays.sort(score);
+
+        for(int i = score.length; i >= m; i -= m){
+            answer += score[i - m] * m;
         }
+
         return answer;
-        
-        
     }
 }
