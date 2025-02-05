75d6268cd4311b1a2d448add059e42b4d61145b4
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-05 17:38:19 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,10 +1,10 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int min_row= wallpaper.length;  //시작점 x값
-        int min_col= wallpaper[0].length();     //시작점 y값
-        int max_row=0;  //종점 x값
-        int max_col=0;  //종점 y값
+        int min_row= Integer.MAX_VALUE;  //시작점 x값
+        int min_col= Integer.MAX_VALUE;     //시작점 y값
+        int max_row= Integer.MIN_VALUE;  //종점 x값
+        int max_col= Integer.MIN_VALUE;  //종점 y값
         //(min_row,min_col,max_row,max_col)
         for(int i=0;i<wallpaper.length;i++){
             String temp=wallpaper[i];
