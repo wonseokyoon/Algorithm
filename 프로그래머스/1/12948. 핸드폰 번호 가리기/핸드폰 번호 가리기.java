943c656daf68f9aedfc6bb02044b8ea1e1498a36
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-09 23:49:32 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.64 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,9 +1,6 @@
+
 class Solution {
   public String solution(String phone_number) {
-     char[] ch = phone_number.toCharArray();
-     for(int i = 0; i < ch.length - 4; i ++){
-         ch[i] = '*';
-     }
-     return String.valueOf(ch);
+    return phone_number.replaceAll(".(?=.{4})", "*");
   }
 }
