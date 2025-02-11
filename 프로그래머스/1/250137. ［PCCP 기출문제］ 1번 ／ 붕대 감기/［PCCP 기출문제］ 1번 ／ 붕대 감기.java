b8e67292adcc5f3ebb2bae9cf7b353fe09874d89
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-11 12:05:07 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.06 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,43 +1,30 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
-        int limit=attacks[attacks.length-1][0]+1;   // 12
-        int[] healths=new int[limit];
-        int t=bandage[0];   // 붕대 감기 시간
-        int x=bandage[1];   // 초당 회복량
-        int y=bandage[2];   // 추가 회복량
-        int seq=0;          // 붕대 연속 감기 시간
-        healths[0]=health;
-        int i=1;
-        for(int j=0;j<attacks.length;j++){
-            int time=attacks[j][0];     // 공격 시점
-            int damage=attacks[j][1];   // 데미지
-            while(i<limit){
-                if(i==time){    // 쳐맞으면
-                    seq=0;  // 붕대 초기화
-                    healths[i]=healths[i-1]-damage;     // 피깎고
-                    if(healths[i]<=0){   // 뒤짐
-                        return -1;
-                    }
-                    i++;
-                    break;
-                }else{
-                    seq++;
-                    healths[i]=healths[i-1]+x;  // 회복
-                    if(seq==t){     // 추가회복
-                        healths[i]=healths[i]+y;
-                        seq=0;  // 초기화
-                    }
-                    if(healths[i]>health){
-                        healths[i]=health;
-                    }
+        int now=health; // 현재 체력
+        int time=bandage[0];    // 붕대감기 시간
+        int dh=bandage[1];      // 초당 회복량
+        int ph=bandage[2];      // 추가 회복량
+        int std=0;  // 마지막으로 공격 당한 시간
+        int interval=0;     // 시간 간격
 
-                    i++;
-                }
+        for(int[] atk:attacks){
+            interval=atk[0]-std-1;    // 시간 간격
+            std=atk[0];    // 공격 시간 업데이트
+            if(interval>=time){
+                now += interval * dh;   // 회복
+                int cnt=interval/time;      // 추가회복을 몇 번 할지
+                now += ph * cnt;          // 추가 회복
+            }else{
+                now+=interval * dh;     // 회복
+            }
+            now = Math.min(now,health);     // 최대 체력 조절
+
+            now-= atk[1];
+            if(now<=0){
+                return -1;
             }
         }
-        return healths[limit-1];
 
-//        int answer = 0;
-//        return answer;
+        return now;
     }
 }
