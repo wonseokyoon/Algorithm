e7e2a3d2a4b007d3357f9ea20c622737dc9d9370
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-12 12:32:27 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 6.77 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,46 +1,43 @@
 import java.util.*;
 class Solution {
+    public boolean check(int row,int col,int size,String[][] park){
+        int x= park.length;
+        int y= park[0].length;
+
+        if(row+size>x||col+size>y){
+            return false;
+        }
+
+        for(int i=row;i<row+size;i++){
+            for(int j=col;j<col+size;j++){
+                if(!park[i][j].equals("-1")){
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
     public int solution(int[] mats, String[][] park) {
         int row = park.length;    // 행
         int col = park[0].length; // 열
-        Arrays.sort(mats); // 돗자리 크기를 오름차순으로 정렬
-        int answer = -1; // 깔 수 있는 최대 크기
+        Arrays.sort(mats);
+        int answer=-1;
 
-        // 돗자리 크기 확인
-        for (int size : mats) {
-            boolean canPlace = false; // 현재 돗자리를 놓을 수 있는지 여부
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(park[i][j].equals("-1")){
 
-            // 공간을 별도로 탐색
-            for (int i = 0; i <= row - size; i++) { // 행 경계
-                for (int j = 0; j <= col - size; j++) { // 열 경계
-                    boolean isAvailable = true; // 해당 위치가 비어 있는지 확인
-                    
-                    // 주어진 크기에서 모든 공간 체크
-                    for (int r = i; r < i + size; r++) {
-                        for (int c = j; c < j + size; c++) {
-                            if (!park[r][c].equals("-1")) { // 다른 사람이 있는 경우
-                                isAvailable = false;
-                                break;
-                            }
+                    for(int matSize:mats){
+                        if(check(i,j,matSize,park)){ // 돗자리 들어가는지
+                            answer=Math.max(matSize,answer);
                         }
-                        if (!isAvailable) break; // 더 이상 확인할 필요 없음
                     }
 
-                    // 비어 있는 경우
-                    if (isAvailable) {
-                        canPlace = true; // 현재 크기의 돗자리를 놓을 수 있는 상태
-                        break;
-                    }
+
                 }
-                if (canPlace) break; // 더 이상 반복할 필요 없음
-            }
-
-            // 최대 크기 업데이트
-            if (canPlace) {
-                answer = size; // 가능한 가장 큰 돗자리를 할당
             }
         }
-
-        return answer; // 결과 반환
+        return answer;
     }
 }
