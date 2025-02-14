b5f2445896814747a055759fef086c750d5fcc77
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-14 11:52:03 +0900

    [level 1] Title: 유연근무제, Time: 0.77 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,48 +1,37 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] schedules, int[][] timelogs, int startday) {
-
-
-        int monday=(8-startday)%7;  // 월요일 인덱스 번호
-        int[][] logs=new int[schedules.length][7];
-        for(int i=0;i<timelogs.length;i++){
-            int[] log=new int[7];
-            for(int j=0;j<7;j++){ // 7회 반복
-                log[j]=timelogs[i][monday++];
-                if(monday==7) monday=0; // 범위 조절
-            }
-            System.out.println(Arrays.toString(log));
-            logs[i]=log;
-        }
-
-        int cnt=schedules.length;   // 총 인원
-        for(int i=0;i<logs.length;i++){
-            int lhour=schedules[i]/100;   // 시
-            int lmin=schedules[i]%100 + 10;   // 분
-            if(lmin>=60){
-                lhour++;
-                lmin-=60;
-            }
-            for (int j=0;j<5;j++){
-                int hour=logs[i][j]/100;
-                int min=logs[i][j]%100;
-                // 늦었으면
-                if(checkToLate(hour,min,lhour,lmin)){
-                    cnt--;
-                    break;
+        int cnt= schedules.length;
+        for(int i=0;i<schedules.length;i++){
+            int day=startday;
+            int y=cal(schedules[i]+10);
+            for(int j=0;j<7;j++){
+                if(day % 7==0 || day % 7==6){
+                    day++;
+                    continue;
+                } else {
+                    // 지각
+                    int x=cal(timelogs[i][j]);
+                    if(x-y > 0){
+                        cnt--;
+                        break;
+                    }
                 }
+                day++;
             }
+
         }
 
         return cnt;
     }
 
-    // 늦으면 참
-    private boolean checkToLate(int hour, int min, int lhour, int lmin) {
-        if(hour>lhour) return true;
-        if(hour==lhour){
-            if(min>lmin) return true;
+    private int cal(int time) {
+        int hour= time / 100;
+        int minute= time % 100;
+        if(minute>=60){
+            hour++;
+            minute-=60;
         }
-        return false;
+        return hour*100+minute;
     }
 }
