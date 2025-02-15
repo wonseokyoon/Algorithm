4d91805d81e108e06c15c169e25210dfe831ebf2
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-02-15 23:56:29 +0900

    [level 1] Title: 내적, Time: 0.20 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
-
 class Solution {
-   public int solution(int[] a, int[] b) {
-        int size=a.length;
-        ArrayList<Integer> answer=new ArrayList<Integer>();
-        for(int i=0;i<size;i++){
-            answer.add(a[i]*b[i]);
+     public int solution(int[] a, int[] b) {
+        int total = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            int multiple = a[i] * b[i];
+            total += multiple;
         }
-        int sum=answer.stream().mapToInt(Integer::intValue).sum();
-        return sum;
+        System.out.println(total);
+        return total;
     }
 }
