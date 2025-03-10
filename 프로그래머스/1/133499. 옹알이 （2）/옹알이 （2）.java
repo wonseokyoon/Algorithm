9ccd0a35807a4c5a3b15ee1e9a9fdddea2521473
wonseokyoon <134195472+wonseokyoon@users.noreply.github.com>
2025-03-10 22:59:53 +0900

    [level 1] Title: 옹알이 (2), Time: 1.44 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,22 +1,15 @@
 class Solution {
-     public int solution(String[] babblings) {
-        int answer=0;
-        for(int i=0;i<babblings.length;i++){
-            if(babblings[i].contains("ayaaya")
-                    || babblings[i].contains("yeye")
-                    || babblings[i].contains("woowoo")
-                    || babblings[i].contains("mama")){
-                continue;
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] filter = {"aya", "ye", "woo", "ma"};
+        for(int i = 0; i < babbling.length;i++){
+            for(int j = 0; j < filter.length; j++){
+                if(!babbling[i].contains("ayaaya") && !babbling[i].contains("yeye") &&!babbling[i].contains("woowoo")&&!babbling[i].contains("mama"))
+                babbling[i] = babbling[i].replace(filter[j]," ");   
             }
-            babblings[i] = babblings[i].replace("aya", " ");
-            babblings[i] = babblings[i].replace("ye", " ");
-            babblings[i] = babblings[i].replace("woo", " ");
-            babblings[i] = babblings[i].replace("ma", " ");
-            babblings[i] = babblings[i].replace(" ", "");
-
-            if(babblings[i].length()  == 0) answer++;
+            babbling[i] = babbling[i].replace(" ", "");
+            if(babbling[i] == "")   answer ++;
         }
         return answer;
-
     }
 }
