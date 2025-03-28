9acb9b43b6f1bb4bb4acd8f7cb7f1ad0611acf0e
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-03-28 21:49:39 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 1.77 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,17 +1,20 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
         String[] params = s.split(" ");
-        int maxRes = Integer.parseInt(params[0]);
-        int minRes = Integer.parseInt(params[0]);
-
-        for(String param : params) {
-            int num = Integer.parseInt(param);
-            if(num > maxRes) {
-                maxRes = num;
-            } else if (num < minRes) {
-                minRes = num;
-            }
+        int[] arrInt = new int[params.length];
+        int i = 0;
+        for(String param : params){
+            arrInt[i++] = Integer.parseInt(param);
         }
-        return minRes + " " + maxRes;
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(Arrays.stream(arrInt).min().getAsInt());
+        sb.append(" ");
+        sb.append(Arrays.stream(arrInt).max().getAsInt());
+
+        return sb.toString();
     }
 }
