5ad8a06d410c0ea532d4194bfd370ec134b388db
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-04-06 20:10:32 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,33 +1,28 @@
-import java.util.*;
 class Solution {
-    public int solution(int[] arr) {
-        int[] num = new int[100];
-        for(int a:arr){
-            Map<Integer,Integer> map = new HashMap<>();
-            int i=2;
-            while(a>1) {
-                if (a % i == 0) {
-                    a /= i;
-                    map.put(i, map.getOrDefault(i, 0) + 1);
-                } else {
-                    i++;
-                }
-            }
-            for(Integer key:map.keySet()){
-                if(num[key] < map.get(key) ){
-                    num[key] = map.get(key);
-                }
-            }
-        }
-        int ans = 1;
-        for(int i =0 ;i<100;i++){
-            if(num[i]>0){
-                for(int j=0;j<num[i];j++){
-                    ans *= i;
-                }
-            }
+        public int solution(int[] arr) {
+        int answer = arr[0];
+        for(int i=1;i<arr.length;i++){
+            answer = lcm(arr[i], answer);
         }
 
-        return ans;
+        return answer;
+    }
+
+    // 최소공배수
+    public int lcm(int a, int b) {
+        int gcd = gcd(a,b);
+        a = a / gcd;
+        b = b / gcd;
+        return a * b * gcd;
+    }
+
+    // 최대공약수
+    public int gcd(int a, int b) {
+        while(b>0){
+            int temp = a%b;
+            a=b;
+            b=temp;
+        }
+        return a;
     }
 }
