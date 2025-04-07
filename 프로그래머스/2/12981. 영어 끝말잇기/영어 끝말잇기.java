5a63fd21b6f61da7e3c964bce79786845727b379
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-04-08 00:57:26 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,29 +1,38 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
-
-        List<String> list = new ArrayList<>();
-        list.add(words[0]);
-
-        for(int i=1; i<words.length; i++){
-            if(list.contains(words[i]) || same(list.get(list.size()-1), words[i])){
-                answer[0] = i%n+1;
-                answer[1] = i/n+1;
-                break;
+public int[] solution(int n, String[] words) {
+        int row = 1;
+        int col = 1;
+        Map<String,Integer> map = new HashMap<>();
+        String prev = "0";
+        for(String word : words){
+            map.put(word,map.getOrDefault(word,0)+1);
+            // 끝말잇기가 실패했거나 단어가 두번 나온 경우
+            if(map.get(word)==2 || !checkWord(prev,word)){
+                return new int[]{col,row};
             }
-            list.add(words[i]);
+            if(col == n){
+                row ++;
+                col-= n-1;
+                prev = word;
+                continue;
+            }
+            col ++;
+            prev = word;
         }
-
-        return answer;
+        return new int[]{0,0};
     }
 
-    public boolean same(String word1, String word2) {
-        if(word1.charAt(word1.length()-1) == word2.charAt(0)){
+    // 성공하면 true 반환
+    public boolean checkWord(String prev,String word) {
+        char end = prev.charAt(prev.length()-1);
+        char start = word.charAt(0);
+        if(end==start || end=='0'){
+            return true;
+        }else{
             return false;
         }
-        return true;
+
     }
 }
