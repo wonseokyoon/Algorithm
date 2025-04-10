ebf84763ac010d1c23286c1d8416c722737644f9
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-04-10 23:03:28 +0900

    [level 2] Title: 괄호 회전하기, Time: 93.28 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,48 +1,42 @@
 import java.util.*;
 class Solution {
 public int solution(String s) {
-
         int len = s.length();
-        Queue<Character> queue = new LinkedList<>();
-        for(char c : s.toCharArray()){
-            queue.offer(c);
-        }
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
         int cnt = 0;
         for(int i=0;i<len;i++){
-            boolean flag = true;
-            // quque의 사본
-            Queue<Character> tempQue = new LinkedList<>(queue);
-            for(int j=0; j<i; j++){
-                // 맨 앞 꺼내서 맨 뒤로 집어넣음
-                char temp = tempQue.poll();
-                tempQue.offer(temp);
-            }
-            Stack<Character> tempStack = new Stack<>();
+            stack.clear();
+            int index;
+            for(int j=0;j<len;j++){
+                // 인덱스
+                index = (i+j) % len;
+                char ch = chars[index];
 
-            for(int j=0; j<len; j++){
-                char ch = tempQue.poll();
-                if(ch == '[' || ch == '{' || ch == '('){
-                    tempStack.push(ch);
-                } else if (ch == ']') {
-                    if(tempStack.isEmpty() || tempStack.pop() != '[') {
-                        flag=false;
-                        break;
+                // 스택에 뭐가 들어있을때만
+                if(!stack.isEmpty()){
+                    // 쩍꿍찾기
+                    if(ch == ']' && stack.peek() == '['){
+                        stack.pop();
+                    } else if(ch == '}' && stack.peek() == '{'){
+                        stack.pop();
+                    } else if(ch == ')' && stack.peek() == '(') {
+                        stack.pop();
                     }
-                } else if(ch == '}'){
-                    if(tempStack.isEmpty() || tempStack.pop() != '{') {
-                        flag=false;
-                        break;
-                    }
-                } else if(ch == ')'){
-                    if(tempStack.isEmpty() || tempStack.pop() != '('){
-                        flag=false;
-                        break;
-                    }
+                    // 해당되는거 없어도 집어넣음: ),],}
+                    else stack.push(ch);
                 }
-            }           
-            if(!tempStack.isEmpty()) flag=false;
-            if(flag) cnt++;
+                // 스택이 비었으면 집어넣음
+                else{
+                    stack.push(ch);
+                }
+            }
+            // 스택을 다 비웠으면
+            if(stack.isEmpty()){
+                cnt ++;
+            }
         }
+
         return cnt;
     }
 
