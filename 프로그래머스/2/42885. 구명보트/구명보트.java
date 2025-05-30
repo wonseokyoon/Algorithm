ecad9d4c71069906080bc3ef19fc1a340d501728
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-05-30 20:30:34 +0900

    [level 2] Title: 구명보트, Time: 10.99 ms, Memory: 55.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,27 +1,13 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+
 class Solution {
-     public int solution(int[] peoples, int limit) {
-        Arrays.sort(peoples);
-        ArrayList<Integer> list = new ArrayList<>();
-        int light = 0;
-        int heavy = peoples.length-1;
-
-        while(light <= heavy) {
-
-            if(peoples[light] + peoples[heavy] > limit){
-                list.add(peoples[heavy]);
-                heavy--;
-            }else {
-                list.add(peoples[heavy] + peoples[light]);
-                light++;
-                heavy--;
-            }
-
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        for (; i < j; --j) {
+            if (people[i] + people[j] <= limit)
+                ++i;
         }
-
-        System.out.println(list);
-
-        return list.size();
+        return people.length - i;
     }
 }
