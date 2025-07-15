6d2ce3670b6a4b7cd050174395f40a07260c9256
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-07-15 09:59:13 +0900

    [level 2] Title: 롤케이크 자르기, Time: 148.98 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,34 +1,22 @@
 import java.util.*;
 class Solution {
-        public int solution(int[] topping) {
-        HashMap<Integer,Integer> left = new HashMap<>();
-        HashMap<Integer,Integer> right = new HashMap<>();
-
+    public int solution(int[] topping) {
+        LinkedHashSet<Integer> left = new LinkedHashSet<>();
+        int[] leftCount = new int[topping.length];
+        LinkedHashSet<Integer> right = new LinkedHashSet<>();
+        int[] rightCount = new int[topping.length];
         int answer = 0;
-
-        // Map을 이용해서 중복 제거와 갯수 카운팅
         for(int i = 0; i < topping.length; i++){
-            left.put(topping[i], left.getOrDefault(topping[i], 0) + 1);
+            left.add(topping[i]);
+            leftCount[i] = left.size();
         }
 
-        for(int i=topping.length-1; i>=1; i--){     // 반대편에서부터 카운트
-            int cur = topping[i];   // 현재 원소
-            right.put(cur, right.getOrDefault(cur, 0) + 1); // 오른쪽에 추가
-            left.put(cur, left.getOrDefault(cur, 0) - 1);   // 왼쪽에서 제거
+        for(int i=topping.length-1; i>=1; i--){
+            right.add(topping[i]);
+            rightCount[i] = right.size();
 
-            if(left.get(cur) == 0){ // 현재 원소 갯수가 0이면 제거
-                left.remove(cur);
-            }
-
-            // 토핑 가짓수 비교
-            if(left.size() == right.size()){
-                answer++;
-            }
-
-            if(left.size() < right.size()){
-                break;
-            }
-
+            if(leftCount[i - 1] == rightCount[i]) answer++;
+            if(leftCount[i - 1] < rightCount[i]) break;
         }
 
         return answer;
