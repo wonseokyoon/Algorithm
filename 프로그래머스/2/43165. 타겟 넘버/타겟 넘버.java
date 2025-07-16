5fa15b69b3ed26188485817be1467412bdc168ea
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-07-16 09:49:06 +0900

    [level 2] Title: 타겟 넘버, Time: 7.78 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -6,12 +6,8 @@ class Solution {
     private int dfs(int[] numbers, int target, int index, int sum) {
         int cnt = 0;
         // 종료 조건 체크        
-        if(index == numbers.length){
-            // 조건에 맞으면 1 반환 아니면 0 반환
-            if(sum == target){
-                return 1;
-            }
-            return 0;
+       if(index == numbers.length){
+            return sum == target ? 1 : 0;
         }
         
         // + 부호 추가, 조건에 해당되면 +1, 아니면 아무일도 안일어남
