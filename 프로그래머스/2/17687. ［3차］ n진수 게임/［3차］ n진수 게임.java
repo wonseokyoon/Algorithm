62017cbb12be2744c326b5aa263d6b111c431e6d
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-07-18 10:18:19 +0900

    [level 2] Title: [3차] n진수 게임, Time: 17.43 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,11 +1,12 @@
 class Solution {
-public String solution(int n, int t, int m, int p) {
+    public String solution(int n, int t, int m, int p) {
         StringBuilder str = new StringBuilder();
         StringBuilder res = new StringBuilder();
         int num = 0;
         int limit = t * m;
+
+        // 10 진수 num 을 n 진법으로 변환
         while(str.length() < limit){
-            // 10 진수 num 을 n 진법으로 변환
             String decimal = Integer.toString(num,n);
             str.append(decimal);
             num++;
@@ -13,13 +14,17 @@ public String solution(int n, int t, int m, int p) {
 
         int turn = 1;
         for(char c : str.toString().toCharArray()){
+            // 나눠 떨어지지 않으면 나머지로 치환
             turn = (turn % m == 0) ? turn : turn % m;
+            // 내차례
             if(turn == p){
                 res.append(c);
+                // 길이 충족시 종료
                 if(res.length() == t) break;
             }
             turn++;
         }
+        // 대문자 치환
         return res.toString().toUpperCase();
     }
 }
