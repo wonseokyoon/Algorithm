cb9e68ad38fba8b77336957d41da0fae1a4d341d
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-07-23 10:25:05 +0900

    [level 2] Title: 택배상자, Time: 164.35 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,40 +1,19 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] order) {
-        Queue<Integer> box = new PriorityQueue<>(); // 택배 박스
+        public int solution(int[] order) {
         Stack<Integer> assist = new Stack<>();  // 보조 벨트
+        int cnt = 0;
 
-        for(int i = 1; i <= order.length; i++){
-            box.add(i);     // 1,2,3,4,...
+        for(int i = 0; i < order.length; i++){
+            assist.push(i+1);
+
+            while(!assist.isEmpty()){
+                if(assist.peek() == order[cnt]){
+                    assist.pop();
+                    cnt++;
+                } else break;
+            }
         }
-
-        int cnt = 0;    // 갯수 카운트
-        int idx = 0;    // order 인덱스 추적
-        // 박스를 전부 비울때까지
-        while(!box.isEmpty() || !assist.isEmpty()){
-
-            // 박스의 값이 order 와 일치 할 경우
-            if(!box.isEmpty() && box.peek() == order[idx]){
-                // 카운팅하고 다음 인덱스로
-                box.poll();
-                cnt++;
-                idx++;
-
-                // order 와 보조 벨트의 값과 일치 할 경우
-            } else if (!assist.isEmpty() && assist.peek() == order[idx]) {
-                cnt++;
-                idx++;
-                assist.pop();
-
-                // order 와 보조 벨트 값이 다른 경우
-            } else if (!box.isEmpty()) {
-                // 보조 벨트에 푸시
-                assist.push(box.poll());
-
-                // 종료
-            } else break;
-        }
-
         return cnt;
     }
 }
