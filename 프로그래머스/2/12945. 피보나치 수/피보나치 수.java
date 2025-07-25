20e482902de2c44e5d0af7c1007930a404166b83
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-07-25 18:19:06 +0900

    [level 2] Title: 피보나치 수, Time: 3.74 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,19 +1,15 @@
 class Solution {
-    public int solution(int n) {
-        int answer = fibo(n);
-        return answer;
-    }
-
-    public int fibo(int num) {
-        if(num == 0) return 0;
-        if(num == 1) return 1;
-
-        int[] fib = new int[num + 1];
-        fib[0] = 0;
-        fib[1] = 1;
-        for(int i = 2; i <= num; i++) {
-            fib[i] = (fib[i - 1] + fib[i - 2]) % 1234567;
+        public int solution(int n) {
+        int answer = 0;
+        long[] pib=new long[n+1];
+        pib[0]=0L;
+        pib[1]=1L;
+        int times=1;
+        for(int i=2; i<=n; i++){
+            pib[i]=(pib[i-1]+pib[i-2])%1234567L;
         }
-        return fib[num];
+        //answer=(int)(pib[n]%1234567L);
+        answer=(int)(pib[n]);
+        return answer;
     }
 }
