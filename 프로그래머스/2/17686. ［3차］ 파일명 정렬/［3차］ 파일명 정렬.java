9151529c9fa907e611242303b41186d0cf3b3d5d
nokkae <134195472+wonseokyoon@users.noreply.github.com>
2025-08-01 10:55:00 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 175.56 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,95 +1,31 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 class Solution {
-static class FileInfo{
-        String original;
-        String head;
-        int number;
-        String tail;
-
-        public FileInfo(String original, String head, int number, String tail) {
-            this.original = original;
-            this.head = head;
-            this.number = number;
-            this.tail = tail;
-        }
-    }
-
     public String[] solution(String[] files) {
+        Pattern pattern = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
 
-        ArrayList<FileInfo> fileInfos = new ArrayList<>();
-
-        for(String file : files) {
-            // 구간 구분
-            String[] sections = splitSection(file);
-            String head = sections[0];
-            String number = sections[1];
-            String tail = sections[2];
-
-            System.out.println("head: " + head);
-            System.out.println("number: " + number);
-            System.out.println("tail: " + tail);
-
-            fileInfos.add(new FileInfo(file, head, Integer.parseInt(number), tail));
-        }
-
-        // 정렬
-        fileInfos = sortFiles(fileInfos);
-        String[] answer = new String[fileInfos.size()];
-        for(int i = 0; i < fileInfos.size(); i++) {
-            answer[i] = fileInfos.get(i).original;
-        }
-
-        return answer;
-    }
-
-    private ArrayList<FileInfo> sortFiles(ArrayList<FileInfo> fileInfos) {
-
-        Collections.sort(fileInfos, new Comparator<FileInfo>() {
+        Arrays.sort(files,new Comparator<String>() {
             @Override
-            public int compare(FileInfo o1, FileInfo o2) {
-                // 1. 헤드 정렬
-                int compare = o1.head.compareToIgnoreCase(o2.head);     // 대소문자 무시
-                if(compare != 0){      // 양수: o1>o2, 음수: o1 < o2, 0: o1=o2
-                    return compare;
+            public int compare(String o1, String o2) {
+                // Mathcher 객체 생성(대소문자 구분x)
+                Matcher matcher1 = pattern.matcher(o1.toLowerCase());
+                Matcher matcher2 = pattern.matcher(o2.toLowerCase());
+
+                // 스캔
+                matcher1.find();
+                matcher2.find();
+
+                // 헤더가 다른 경우
+                if(!matcher1.group(1).equals(matcher2.group(1))){
+                    return matcher1.group(1).compareTo(matcher2.group(1));
                 }
-                // 2. 헤드가 같으면 number 정렬
-                return Integer.compare(o1.number, o2.number);
+
+                // Number로 정렬
+                return Integer.parseInt(matcher1.group(2)) - Integer.parseInt(matcher2.group(2));
             }
         });
-
-        return fileInfos;
-    }
-
-    private String[] splitSection(String file) {
-        StringBuilder head = new StringBuilder();
-        StringBuilder number = new StringBuilder();
-        StringBuilder tail = new StringBuilder();
-
-        int i = 0;
-
-        // Head
-        for(; i < file.length(); i++) {
-            char c = file.charAt(i);
-            if(Character.isDigit(c)) break;
-            head.append(c);
-        }
-
-        // Number
-        for(; i < file.length(); i++) {
-            char c = file.charAt(i);
-            if(!Character.isDigit(c) || number.length() >= 5) break;
-            number.append(c);
-        }
-
-        // Tail
-        for(; i < file.length(); i++) {
-            char c = file.charAt(i);
-            tail.append(c);
-        }
-
-        return new String[]{head.toString(), number.toString(), tail.toString()};
+        return files;
     }
 }
