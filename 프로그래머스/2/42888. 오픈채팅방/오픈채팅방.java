e9d356639d1872aa5279c7e5d9341bba218145af
nokkae <01088115030a@gmail.com>
2025-08-02 09:36:31 +0900

    [level 2] Title: 오픈채팅방, Time: 185.01 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,36 +1,52 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 class Solution {
+        class User {
+        String id;
+        String nickname;
+
+        User(String id,String nickname){
+            this.id = id;
+            this.nickname = nickname;
+        }
+
+        public void setNickname(String nickname) {
+            this.nickname = nickname;
+        }
+
+        public String getNickname() {
+            return nickname;
+        }
+    }
+
     public String[] solution(String[] record) {
-//        Pattern pattern = Pattern.compile("([a-z\\s.-]+)([0-9]{1,5})");
-        Map<String,String> map = new HashMap<>();
+        Map<String, User> map = new HashMap<>();
 
         for(String str : record) {
             String[] split = str.split(" ",3);
+            String type = split[0];
             String id = split[1];
-            if(split.length > 2) {
+            if(type.equals("Enter")) {
                 String nickname = split[2];
-                map.put(id,nickname);
+                map.put(id, new User(id,nickname));
+            } else if (type.equals("Change")) {
+                String nickname = split[2];
+                map.get(id).setNickname(nickname);  // 닉네임 수정
             }
-
         }
 
-        StringBuilder sb = new StringBuilder();
+        ArrayList<String> result = new ArrayList<>();
         for(String str : record) {
             String type = str.split(" ",2)[0];
             String id = str.split(" ",3)[1];
 
             // 입장,퇴장만 기록
             if(type.equals("Enter")) {
-                sb.append(map.get(id));
-                sb.append("님이 들어왔습니다.,");
+                result.add(map.get(id).getNickname() +"님이 들어왔습니다.");
             } else if (type.equals("Leave")) {
-                sb.append(map.get(id));
-                sb.append("님이 나갔습니다.,");
+                result.add(map.get(id).getNickname() +"님이 나갔습니다.");
             }
         }
-
-        return sb.toString().split(",");
+        return result.toArray(new String[0]);
     }
 }
