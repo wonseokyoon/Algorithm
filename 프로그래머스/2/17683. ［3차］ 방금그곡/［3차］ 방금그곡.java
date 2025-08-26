ebfa0e95299e10fecd1687f071e7a7f936335199
nokkae <backend.wonseok@gmail.com>
2025-08-26 17:36:21 +0900

    [level 2] Title: [3차] 방금그곡, Time: 26.28 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -1,59 +1,47 @@
 import java.util.*;
 import java.time.*;
-
 class Solution {
     public String solution(String m, String[] musicinfos) {
-        // #을 치환
+        // # 처리
         m = replaceShap(m);
-        
         int maxTime = 0;
         String answer = "(None)";
         
-        for(String musicinfo: musicinfos){
+        for(String musicinfo:musicinfos){
             String[] params = musicinfo.split(",");
+            LocalTime startTime = LocalTime.parse(params[0]);
+            LocalTime endTime = LocalTime.parse(params[1]);
+            int playTime = (int) Duration.between(startTime,endTime).toMinutes();
+            
             String title = params[2];
             String info = replaceShap(params[3]);
-            
-            LocalTime start = LocalTime.parse(params[0]);
-            LocalTime end = LocalTime.parse(params[1]);
-            // 시간차
-            int time = (int) Duration.between(start,end).toMinutes();
-            
-            StringBuilder sb = new StringBuilder();
-            char[] ch = info.toCharArray();
+            StringBuilder fullMelody = new StringBuilder();
             int length = info.length();
-            for(int i=0;i<time;i++){
-                sb.append(ch[i%length]);
+            // 재생 시간동안 나온 전체 악보
+            for(int i = 0; i < playTime; i++){
+                fullMelody.append(info.charAt(i % length));
             }
+            System.out.println("풀멜로디: " + fullMelody);
             
-            // 유사성 체크
-            if(checkCommon(m,sb.toString())){
-                
-                // 새로운게 더 길면 -> 덮어씌움
-                if(time > maxTime){
-                    maxTime = time;
+            // 전체 악보에 기억중인 멜로디가 포함되어있다면
+            if(fullMelody.toString().contains(m)){
+                if(playTime > maxTime){
                     answer = title;
+                    maxTime = playTime;
                 }
             }
         }
-        
-        // 제목 반환
         return answer;
     }
     
-    // 유사성 체크
-    boolean checkCommon(String m,String info){
-        // 멜로디가 info에 포함되는지
-        if(info.contains(m)) return true;
-        return false;
-    }
-    
-    String replaceShap(String info){
-        return info.replaceAll("C#","H")
-            .replaceAll("D#","I")
-            .replaceAll("F#","J")
-            .replaceAll("G#","K")
-            .replaceAll("A#","L")
-            .replaceAll("B#","M");
+    String replaceShap(String str){
+        return str
+            .replaceAll("A#","H")
+            .replaceAll("B#","I")
+            .replaceAll("C#","J")
+            .replaceAll("D#","K")
+            .replaceAll("E#","L")
+            .replaceAll("F#","M")
+            .replaceAll("G#","N");
     }
 }
