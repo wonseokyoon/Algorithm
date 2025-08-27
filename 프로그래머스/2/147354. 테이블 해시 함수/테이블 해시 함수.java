af36a48ffd68f64795c140a6c8d2110ad8e0695f
nokkae <backend.wonseok@gmail.com>
2025-08-27 20:27:26 +0900

    [level 2] Title: 테이블 해시 함수, Time: 8.67 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -2,35 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        
         Arrays.sort(data,(a,b) -> {
-            if(a[col-1] == b[col-1]){
-                return b[0] - a[0];         // 첫뻔재 컬럼을 기준으로 내림차 정렬 
-            } else{
-                return a[col-1] - b[col-1];         // col-1 컬럼을 기준으로 오름차 정렬 
+            if(a[col-1] == b[col-1]) {  // 같으면
+                return b[0] - a[0];     // 첫번째로 내림차순 정렬
             }
+            return a[col-1] - b[col-1];
         });
         
-        ArrayList<Integer> siList = new ArrayList<>();
-        for(int i=row_begin-1;i<=row_end-1;i++){
-            int[] d = data[i];
-            int si = calSI(d,i+1);
-            System.out.println("si: "+si);
-            siList.add(si);
+        int diff = row_end - row_begin;
+        int[] sIList = new int[diff+1];
+        int si = 0;
+        for(int i=row_begin-1; i<=row_end-1;i++){
+            int[] row = data[i];
+            si ^= calSI(row,i+1);       // SI 계산
         }
-        int answer = siList.get(0);
-        for(int i=1;i<siList.size();i++){
-            answer ^= siList.get(i);
-        }
-        
-        return answer;
+        return si;
     }
     
-    int calSI(int[] datas,int row) {
-        int sum =0;
-        for(int data : datas){
-            sum += data % row;         
-        }
-        return sum;
+    int calSI(int[] row,int idx){
+        int si = 0;
+        for(int data:row){
+            si += data%idx;
+        }                  
+        return si;
     }
 }
