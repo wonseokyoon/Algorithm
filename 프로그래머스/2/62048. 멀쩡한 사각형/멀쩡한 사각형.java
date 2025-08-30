ddc9697fbaa5c52fcde6a281ade02a472fa7b483
nokkae <backend.wonseok@gmail.com>
2025-08-30 11:15:47 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 1.57 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,22 +1,14 @@
 import java.util.*;
+import java.math.BigInteger;
 
 class Solution {
     public long solution(int w, int h) {
-        // 곱셈을 하기 전에 long으로 형변환하여 오버플로우를 방지합니다.
         long extent = (long) w * h;
-        long gcd = GCD(w, h);      // 최대공약수
-        long cnt = (long) w + h - gcd; // 가독성을 위해 (long)w 추가 (필수는 아님)
-
-        return extent - cnt;
+        BigInteger bigW = BigInteger.valueOf(w);
+        BigInteger bigH = BigInteger.valueOf(h);
+        long gcd = bigW.gcd(bigH).longValue();
+        long cnt = w + h - gcd;
+        return extent - cnt;        
     }
-
-    // 유클리드 호제법 (매우 효율적이며 올바르게 작성하셨습니다)
-    long GCD(long num1, long num2){
-        while(num2 != 0){
-            long r = num1 % num2;
-            num1 = num2;
-            num2 = r;
-        }
-        return num1;
-    }
+    
 }
