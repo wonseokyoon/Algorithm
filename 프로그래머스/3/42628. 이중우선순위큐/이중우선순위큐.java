e01f4822ccb30c755cf7e36b2f0bf80a97b40d65
nokkae <backend.wonseok@gmail.com>
2025-08-30 12:37:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 94.37 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,37 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
+        // 오름차순 우선순위 큐
+        PriorityQueue<Integer> ascPq = new PriorityQueue<>();
+        // 내림차순 우선순위 큐
+        PriorityQueue<Integer> descPq = new PriorityQueue<>((a,b) -> b-a);
         
-        for(String operation : operations){
-            String command = operation.split(" ")[0];
-            int number = Integer.parseInt(operation.split(" ")[1]);
-            
-            if(command.equals("I")){
-                map.put(number,map.getOrDefault(number,0) +1);
-            } else{
-                if(map.size() > 0){
-                    if(number == -1) {  // 최솟값 제거
-                        int firstKey = map.firstKey();
-                        int cnt = map.get(firstKey);
-                        if(cnt > 1) {
-                            map.put(firstKey,cnt - 1);
-                        } else{
-                            map.remove(firstKey);
-                        }
-                    } else{             // 최댓값 제거
-                        int lastKey = map.lastKey();
-                        int cnt = map.get(lastKey);
-                        if(cnt > 1) {
-                            map.put(lastKey,cnt - 1);
-                        } else{
-                            map.remove(lastKey);
-                        }
+        int size = 0;
+        for(String operation: operations){
+            String[] params = operation.split(" ");
+            String command = params[0]; // 명령어
+            String number = params[1];  // 숫자
+            if(command.equals("I")){   // 삽입 명령어
+                ascPq.offer(Integer.parseInt(number));
+                descPq.offer(Integer.parseInt(number));
+                size ++;
+            } else {    // 삭제 명령어
+                if(size > 0){
+                    if(number.equals("-1")) {   // 최솟값 삭제
+                        descPq.remove(ascPq.poll());
+                        size --;
+                    } else {        // 최댓값 삭제
+                        ascPq.remove(descPq.poll());
+                        size --;
                     }
                 }
             }
         }
-        if(map.size() == 0) return new int[]{0,0};
-        return new int[]{map.lastKey(),map.firstKey()};
+        if(size  == 0) return new int[]{0,0};
+        return new int[]{descPq.poll(),ascPq.poll()};
     }
 }
