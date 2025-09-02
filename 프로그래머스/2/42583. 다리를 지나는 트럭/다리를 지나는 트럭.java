6ce011ad19badcee01b6f7581474a14ef1c42a47
nokkae <backend.wonseok@gmail.com>
2025-09-02 11:07:28 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 16.70 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,48 +1,34 @@
 import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<>();
-        int time = 0;
-        int idx = 0;
-        while(idx < truck_weights.length){
-            if(queue.isEmpty()){
-                queue.add(truck_weights[idx++]);
-                time++;
-                continue;
+        Queue<Integer> readyQueue = new LinkedList<>();
+        // [무게,경과시간]
+        Queue<int[]> bridgeQueue = new LinkedList<>();
+        for(int truck :truck_weights){
+            readyQueue.offer(truck);
+        }
+        
+        int weightSum = 0;
+        int currentTime = 0;
+        int startTime;
+        while(!readyQueue.isEmpty()){
+            currentTime ++;
+            int truck = readyQueue.peek();
+            if(!bridgeQueue.isEmpty()){
+                startTime = bridgeQueue.peek()[1];  // 다리에 올라간 시간
+                if(currentTime - startTime == bridge_length){
+                    // 완전히 다리에서 내려감
+                    weightSum -= bridgeQueue.poll()[0];             
+                }
             }
-
-            // 다리 길이보다 짧을 경우
-            if(queue.size() < bridge_length){
-                // 큐의 합
-                long current = queue.stream()
-                        .mapToLong(Integer::longValue)
-                        .sum();
-
-                // 하중 초과
-                if(current + truck_weights[idx] > weight){
-                    queue.offer(0);     // 다리의 시간 흐름 체크를 위해 빈 공간 추가
-                    time++;
-                }else{
-                    queue.add(truck_weights[idx++]);
-                    time++;
-                }
-            } else if (queue.size() == bridge_length) {
-                queue.poll();   // 내림
-
-                long current = queue.stream()
-                        .mapToLong(Integer::longValue)
-                        .sum();
-                if(current + truck_weights[idx] > weight){
-                    queue.offer(0);
-                    time++;
-                }else{
-                    queue.add(truck_weights[idx++]);
-                    time++;
-                }
+            
+            if(truck + weightSum <= weight){
+                bridgeQueue.offer(new int[]{truck,currentTime});   // 다리에 올림
+                weightSum+=truck;           // 무게 추가
+                readyQueue.poll();
             }
         }
-
-
-        return time + bridge_length;
+        
+        return currentTime + bridge_length;
     }
 }
