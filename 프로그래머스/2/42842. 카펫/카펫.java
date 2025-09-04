b2f6190b78d522af625320704d642c65ab885889
nokkae <backend.wonseok@gmail.com>
2025-09-04 19:10:46 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,9 +1,17 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int brown, int red) {
-        int a = (brown+4)/2;
-        int b = red+2*a-4;
-        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
+    public int[] solution(int brown, int yellow) {
+        int sum = brown + yellow;
+        
+        for(int height = 3; height <= Math.sqrt(sum); height++){
+            if(sum % height == 0){
+                int width = sum / height;
+                if((width - 2) * (height - 2) == yellow){
+                    return new int[]{width,height};
+                }
+            }
+        }
+        int[] answer = {};
         return answer;
     }
 }
