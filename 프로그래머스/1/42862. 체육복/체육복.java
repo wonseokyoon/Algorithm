2abc6bb0339ad3cca7dcfe82c1829b42cc327837
nokkae <backend.wonseok@gmail.com>
2025-09-05 13:06:15 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,25 +1,34 @@
+import java.util.*;
 class Solution {
-    public int solution(int n, int[] lost, int[] reserve) {
-        int[] people = new int[n];
+    public int solution(int n, int[] losts, int[] reserves) {
+        int[] student = new int[n];
+        Arrays.fill(student,1);
+        
         int answer = n;
+        
+        for(int lost : losts) {
+            student[lost-1] --;
+        }
+        
+        for(int reserve : reserves){
+            student[reserve-1] ++;
+        }
 
-        for (int l : lost) 
-            people[l-1]--;
-        for (int r : reserve) 
-            people[r-1]++;
-
-        for (int i = 0; i < people.length; i++) {
-            if(people[i] == -1) {
-                if(i-1>=0 && people[i-1] == 1) {
-                    people[i]++;
-                    people[i-1]--;
-                }else if(i+1< people.length && people[i+1] == 1) {
-                    people[i]++;
-                    people[i+1]--;
-                }else 
-                    answer--;
+        for(int i = 0 ; i < n; i ++){
+            if(student[i] == 0){
+                if(i > 0 && student[i-1] == 2) {     // 왼쪽에서 빌림
+                    student[i-1] --;
+                    student[i] ++;
+                } else if(i < n - 1 && student[i+1] == 2) {   // 오른쪽에서 빌림
+                    student[i+1] --;
+                    student[i] ++;
+                } else{
+                    answer --;
+                }
             }
         }
+        
+        
         return answer;
     }
 }
