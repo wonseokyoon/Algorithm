7e4f5c46d8eb991df2cb1db17d43a41fbbad7b52
nokkae <backend.wonseok@gmail.com>
2025-09-06 17:44:25 +0900

    [level 2] Title: H-Index, Time: 17.91 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,30 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        // n편의 논문 중, h번 이상 인용된 논문이 h 이상, 나머지 논문이 h번 이하 인용되었다면 h의 최댓값: h-index
+        int len = citations.length;
         
         int[] sorted = Arrays.stream(citations)
             .boxed()
             .sorted(Collections.reverseOrder())
             .mapToInt(Integer::intValue)
             .toArray();
+        
         System.out.println(Arrays.toString(sorted));
         
-        int len = citations.length;     // 길이
-        int hIdx = sorted[0];      // 최댓값으로 초기값 설정
-        int max = 0;
-        while(hIdx > 0) {
-            int cnt = 0;    // h번 이상 인용된 논문 갯수
+        int h = sorted[0];
+        while(h > 0){
+            int cnt = 0;
             for(int i = 0; i < len; i++){
-                if(sorted[i] >= hIdx) {
-                    cnt ++;
-                } else{
-                    break;
-                }
+                if(sorted[i] >= h) cnt ++;
+                else break;
             }
-            if(cnt >= hIdx) return hIdx;
-            hIdx --;
+            
+            if(cnt >= h) return h;
+            else{
+                h--;
+            }
         }
-        return 0;
+        
+        int answer = 0;
+        return answer;
     }
 }
