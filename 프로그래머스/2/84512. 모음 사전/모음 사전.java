59322661f38bfa2aad093c78edb13f37abf7793b
nokkae <backend.wonseok@gmail.com>
2025-09-08 18:59:57 +0900

    [level 2] Title: 모음 사전, Time: 2.74 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,29 @@
 import java.util.*;
+
 class Solution {
-    public char[] words = {'A', 'E', 'I', 'O', 'U'};
-    public ArrayList<String> dictionary = new ArrayList<>();
-    
+    public char[] words = { 'A', 'E', 'I', 'O', 'U'};
+    ArrayList<String> list = new ArrayList<>();
     public int solution(String word) {
         
-        dfs(new StringBuilder(),0);
-        int answer = dictionary.indexOf(word);
+        dfs(new StringBuilder(), 0,word);
         
+        int answer = list.indexOf(word) + 1;
         return answer;
     }
     
-    void dfs(StringBuilder current,int len) {
-        if(len > 5) return;
-        
-        dictionary.add(current.toString());
+    public void dfs(StringBuilder sb , int length, String target){
+        if(length > 0) {
+            if(length == 5) {
+                list.add(sb.toString());
+                return;
+            }
+            list.add(sb.toString());
+        }
         
         for(char word : words) {
-            dfs(current.append(word), len+1);
-            current.setLength(len);
+            dfs(sb.append(word),length + 1,target);
+            sb.setLength(length);
         }
+        
     }
 }
