3dfa777e775ac3c3de352d3f7ec9d0f6f9210934
nokkae <backend.wonseok@gmail.com>
2025-09-10 21:40:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 83.76 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,18 +1,15 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] elements) {
-        int size = elements.length;
-        Map<Integer,Integer> map = new HashMap<>();
-
-        for(int i = 0; i < size; i++){
-            int sum = 0;
-            for(int j = 0; j < size; j++){
-                sum += elements[(i+j)%size];
-                map.put(sum, map.getOrDefault(sum, 0));
+        public int solution(int[] elements) {
+            Set<Integer> set = new HashSet<>();
+            int[] dp = new int[elements.length];
+            for(int len = 1;len <= elements.length; len++){
+                for(int i = 0;i<elements.length;i++){
+                    dp[i] += elements[(len+i-1)%elements.length];
+                    set.add(dp[i]);
+                }
             }
+            return set.size();
         }
-
-        return map.size();
     }
-}
