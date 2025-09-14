04d55d02a5a15833b7e58a1a6fc502fc96abc8d9
nokkae <backend.wonseok@gmail.com>
2025-09-14 23:43:37 +0900

    [level 2] Title: 프로세스, Time: 1.10 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,51 +1,35 @@
 import java.util.*;
+
 class Solution {
-    class Status {
-        int priority;
-        int location;
-        
-        public Status(int priority, int location){
-            this.priority = priority;
-            this.location = location;
-        }
-        
-        @Override
-        public String toString(){
-            return "[" + priority + "," + location +"]";
-        }
-    }
-    
     public int solution(int[] priorities, int location) {
-        Queue<Status> queue = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        int i = 0;
-        for(int priority : priorities){
-            Status status = new Status(priority,i++);
-            queue.offer(status);
-            list.add(priority);
+        int answer = 0;
+        int l = location;
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i : priorities){
+            que.add(i);
         }
-        
-        // 정렬
-        Collections.sort(list,Collections.reverseOrder());
-        
-        int cnt = 1;
-        while(!queue.isEmpty()){
-            Status status = queue.poll();
-            int priority = status.priority;
-            int idx = status.location;
-            int target = list.get(0);
-            
-            if(target == priority) {
-                list.remove(0);    // 삭제
-                if(location == idx) return cnt;
-                cnt ++;
-            } else{
-                queue.offer(status);
+
+        Arrays.sort(priorities);
+        int size = priorities.length-1;
+
+
+
+        while(!que.isEmpty()){
+            Integer i = que.poll();
+            if(i == priorities[size - answer]){
+                answer++;
+                l--;
+                if(l <0)
+                    break;
+            }else{
+                que.add(i);
+                l--;
+                if(l<0)
+                    l=que.size()-1;
             }
         }
-        
-        
-        int answer = 0;
+
         return answer;
     }
 }
