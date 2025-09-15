b2c2cf0d8d9544cc119b9888bfc1150f67486848
nokkae <backend.wonseok@gmail.com>
2025-09-15 17:40:25 +0900

    [level 3] Title: N으로 표현, Time: 23.63 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,44 +1,42 @@
 import java.util.*;
 
 class Solution {
-    HashSet<Integer> set = new HashSet<>();
-    ArrayList<HashSet<Integer>> dp = new ArrayList<>();
     
     public int solution(int N, int number) {
+        ArrayList<HashSet<Integer>> dp = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
         
-        for(int i = 0; i< 9; i++){
+        for(int i = 0; i < 9; i ++) {
             dp.add(new HashSet<>());            
         }
         
-        for(int i = 1; i< 9; i++){
-            Set<Integer> currentSet = dp.get(i);
-            
+        for(int i = 1; i < 9; i++){
+            HashSet<Integer> currentSet = dp.get(i);
             StringBuilder sb = new StringBuilder();
-            
-            // 이어 붙이기
-            for(int j = 0; j < i; j++){
+            for(int j = 0; j < i; j ++){
                 sb.append(N);
             }
+            
             currentSet.add(Integer.parseInt(sb.toString()));
             
             // 사칙연산
-            for(int j = 0; j < i; j++){
-                Set<Integer> setJ = dp.get(j);
-                Set<Integer> setK = dp.get(i-j);
+            for(int j = 1; j < i; j++) {
+                HashSet<Integer> setJ = dp.get(j);
+                HashSet<Integer> setK = dp.get(i-j);
                 
                 for(int num1 : setJ){
                     for(int num2 : setK){
                         currentSet.add(num1 + num2);
-                        currentSet.add(num1 - num2);
-                        currentSet.add(num1 * num2);
-                        if(num2 != 0) currentSet.add(num1 / num2);
+                        currentSet.add(num1 - num2);    
+                        currentSet.add(num1 * num2);    
+                        if(num2 != 0) currentSet.add(num1 / num2);    
                     }
                 }
             }
             
             if(currentSet.contains(number)) return i;
-            
         }
+        
         return -1;
     }
 }
