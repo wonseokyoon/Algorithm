17578fe21f138396ada90fed30a58902c1833348
nokkae <backend.wonseok@gmail.com>
2025-09-16 10:29:18 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 9.08 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,39 +3,44 @@ import java.util.*;
 class Solution {
     boolean[] visited;
     public int solution(int n, int[][] wires) {
+
         visited = new boolean[n+1];
-        int answer = n;
+        int min = n;
+        
+        for(int[] wire : wires) {
+            int towerA = wire[0];
+            int towerB = wire[1];
+            int cnt = 0;
+            
+            visited[towerA] = true;
+            visited[towerB] = true;
+            
+            cnt += dfs(towerA,wires);
+            
+            visited[towerA] = false;
+            visited[towerB] = false;
+            
+            min = Math.min(min, Math.abs(n - cnt - cnt));
+        }
+        
+        return min;
+    }
+    
+    int dfs(int target, int[][] wires){
+        
         int cnt = 1;
         for(int[] wire : wires) {
             int towerA = wire[0];
             int towerB = wire[1];
-
-            // towerA와 연결된 타워의 갯수
-            visited[towerA] = true;
-            visited[towerB] = true;
-            cnt = dfs(towerA,wires);
-            visited[towerA] = false;
-            visited[towerB] = false;
             
-            answer = Math.min(Math.abs((n - cnt) - cnt),answer);
-        }
-        return answer;
-    }
-    
-    public int dfs(int towerA, int[][] wires){
-        int cnt = 1;
-        
-        for(int i = 0; i < wires.length; i++) {
-            int[] wire = wires[i];
-            
-            if(towerA == wire[0] && !visited[wire[1]]){
-                visited[wire[1]] = true;
-                cnt += dfs(wire[1], wires);
-                visited[wire[1]] = false;
-            } else if(towerA == wire[1] && !visited[wire[0]]){
-                visited[wire[0]] = true;
-                cnt += dfs(wire[0], wires);
-                visited[wire[0]] = false;
+            if(target == towerA && !visited[towerB]) {
+                visited[towerB] = true;
+                cnt += dfs(towerB,wires);                
+                visited[towerB] = false;
+            } else if(target == towerB && !visited[towerA]){ 
+                visited[towerA] = true;
+                cnt += dfs(towerA,wires);                
+                visited[towerA] = false;
             }
         }
         
