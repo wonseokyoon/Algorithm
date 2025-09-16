ba7bb6fbff8a4078f36b1898d910d0b1f31dc38f
nokkae <backend.wonseok@gmail.com>
2025-09-16 09:37:15 +0900

    [level 3] Title: 이중우선순위큐, Time: 102.62 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,36 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        // 오름차순: 최솟값
-        PriorityQueue<Integer> ascPq = new PriorityQueue<>();
-        // 내림차순: 최댓값
-        PriorityQueue<Integer> descPq = new PriorityQueue<>((a,b) -> b - a);
+        PriorityQueue<Integer> ascpq = new PriorityQueue<>();       // 오름차순(최솟값)
+        PriorityQueue<Integer> descpq = new PriorityQueue<>((a,b) -> b - a);    // 내림차순(최댓값)
         
         int size = 0;
-        for(String operation : operations){
-            String commend = operation.split(" ")[0];
-            int num = Integer.parseInt(operation.split(" ")[1]);
+        for(String operation : operations) {
+            String command = operation.split(" ")[0];
+            String numStr = operation.split(" ")[1];
+            int num = Integer.parseInt(numStr);
             
-            if(commend.equals("I")){
-                ascPq.offer(num);
-                descPq.offer(num);
+            if(command.equals("I")){
+                ascpq.offer(num);
+                descpq.offer(num);
                 size ++;
-            } else{
+            } else {
                 if(size > 0) {
-                    if(num == -1) {  // 최솟값 삭제
-                        descPq.remove(ascPq.poll());
+                    if(num == -1) { // 최솟값 삭제
+                        descpq.remove(ascpq.poll());
                         size --;
-                    } else {    // 최댓값 삭제
-                        ascPq.remove(descPq.poll());
+                    } else {
+                        ascpq.remove(descpq.poll());
                         size --;
                     }
                 }
             }
         }
-        if(size > 0){
-            return new int[] {descPq.poll(),ascPq.poll()};
-        } else{
+        
+        if(size == 0) {
             return new int[] {0,0};
         }
+        
+        return new int[] {descpq.poll(), ascpq.poll()};
     }
 }
