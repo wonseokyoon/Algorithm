7db0a6c83a0440af6c6bc6e16f932be254d6e636
nokkae <backend.wonseok@gmail.com>
2025-09-16 10:02:22 +0900

    [level 2] Title: 가장 큰 수, Time: 443.88 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,27 +2,28 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // 사전 순 정렬
-        ArrayList<String> strList = new ArrayList<>();
-        for(int number : numbers){
-            strList.add(String.valueOf(number));
-        }
-        Collections.sort(strList,new Comparator<String>(){
-            @Override
-            public int compare(String s1, String s2){
-                int num1 = Integer.parseInt(s1+s2);
-                int num2 = Integer.parseInt(s2+s1);
-                
-                return num2 - num1;
-            }
+        
+        PriorityQueue<String> pq = new PriorityQueue<>(new Comparator<String>(){
             
+            @Override
+            public int compare(String a, String b) {
+                Integer s1 = Integer.parseInt(a+b);
+                Integer s2 = Integer.parseInt(b+a);
+                return s2 - s1;                
+            }
         });
         
-        StringBuilder stringBuilder = new StringBuilder();
-        for(String str : strList){
-            stringBuilder.append(str);
+        for(int number : numbers) {
+            pq.offer(number+"");
         }
-        if(stringBuilder.toString().startsWith("0")) return "0";
+        
+        if(pq.peek().equals("0")) return "0";
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        while(!pq.isEmpty()) {
+            stringBuilder.append(pq.poll());
+        }
+        
         return stringBuilder.toString();
     }
 }
