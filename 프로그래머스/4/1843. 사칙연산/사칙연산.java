2921c9ee671ed0d872ba7ed480dcb8ab062d75f3
nokkae <backend.wonseok@gmail.com>
2025-09-19 22:47:18 +0900

    [level 4] Title: 사칙연산, Time: 24.20 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -2,53 +2,55 @@ import java.util.*;
 
 class Solution {
     public int solution(String arr[]) {
-        ArrayList<Integer> nums = new ArrayList<>();
+        ArrayList<Integer> numbers = new ArrayList<>();
         ArrayList<String> commends = new ArrayList<>();
         
-        for(String str : arr){ 
+        for(String str : arr){
             try {
-                nums.add(Integer.parseInt(str));
-            } catch(Exception e) {
+                numbers.add(Integer.parseInt(str));
+            } catch(Exception e){
                 commends.add(str);
-            }
+            }    
         }
         
-        int cnt = nums.size();
+        int cnt = numbers.size();
         
-        int[][] maxDp = new int[cnt][cnt];
-        int[][] minDp = new int[cnt][cnt];
+        int[][] max = new int[cnt][cnt];
+        int[][] min = new int[cnt][cnt];
         
-        for(int i = 0; i < cnt; i ++) {
-            maxDp[i][i] = nums.get(i);
-            minDp[i][i] = nums.get(i);
+        for(int i = 0; i < cnt; i++){
+            max[i][i] = numbers.get(i);
+            min[i][i] = numbers.get(i);
         }
         
-        for(int length = 2; length <= cnt; length ++) {     // 연산 길이
-            for(int start = 0; start <= cnt - length; start ++) {   // 시작
-                int end = start + length - 1;       // 끝
+        for(int len = 2 ; len <= cnt ; len ++){
+            
+            for(int start = 0; start <= cnt - len; start++) {
+                int end = start + len - 1;
                 
-                maxDp[start][end] = Integer.MIN_VALUE;
-                minDp[start][end] = Integer.MAX_VALUE;
+                max[start][end] = Integer.MIN_VALUE;
+                min[start][end] = Integer.MAX_VALUE;
                 
-                for(int i = start; i < end; i ++) {      // 연산자 위치(분할 위치)
+                for(int i = start; i < end; i ++){
                     String commend = commends.get(i);
-                    
-                    if(commend.equals("+")) {
+                    if(commend.equals("+")){
                         // 최댓값 + 최댓값
-                        maxDp[start][end] = Math.max(maxDp[start][end], maxDp[start][i] + maxDp[i+1][end]);
+                        max[start][end] = Math.max(max[start][end], max[start][i] + max[i+1][end]);
                         // 최솟값 + 최솟값
-                        minDp[start][end] = Math.min(minDp[start][end], minDp[start][i] + minDp[i+1][end]);
+                        min[start][end] = Math.min(min[start][end], min[start][i] + min[i+1][end]);
+                        
                     } else{
                         // 최댓값 - 최솟값
-                        maxDp[start][end] = Math.max(maxDp[start][end], maxDp[start][i] - minDp[i+1][end]);
+                        max[start][end] = Math.max(max[start][end], max[start][i] - min[i+1][end]);
                         // 최솟값 - 최댓값
-                        minDp[start][end] = Math.min(minDp[start][end], minDp[start][i] - maxDp[i+1][end]);
+                        min[start][end] = Math.min(min[start][end], min[start][i] - max[i+1][end]);
                     }
                 }
+                
             }
         }
         
-        int answer = maxDp[0][cnt-1];
-        return answer;
+        
+        return max[0][cnt-1];
     }
 }
