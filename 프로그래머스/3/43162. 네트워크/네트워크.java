fd9da37302ae77f2efb717700de51eb9729e08e4
nokkae <backend.wonseok@gmail.com>
2025-09-22 22:42:00 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,27 +4,27 @@ class Solution {
     boolean[] visited;
     public int solution(int n, int[][] computers) {
         visited = new boolean[n];
+        
         int answer = 0;
-        for(int i = 0; i < n; i ++){
-            if(!visited[i]){
+        
+        for(int i= 0; i < n; i ++){
+            if(!visited[i]) {
                 dfs(i,computers);
-                answer ++;
+                answer++;
             }
         }
         
         return answer;
     }
     
-    void dfs(int idx , int[][] computers) {
+    void dfs(int node,int[][] computers){
         
-        int[] computer = computers[idx];
-        
-        for(int i =0; i < computer.length; i ++) {
-            // 연결된 컴퓨터를 찾음
-            if(computer[i] == 1 && !visited[i]) {
-                visited[idx] = true;
+        int[] computer = computers[node];
+        for(int i = 0 ; i < computer.length; i++) {
+            if(!visited[i] && computer[i] == 1){
+                visited[i] = true;
                 dfs(i,computers);
-            } 
+            }
         }
     }
 }
